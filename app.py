77e945e8ebbe93a9df7eb868f60d8315dfc3ab9d
Hiroaki Ogasawara <hiroga1030@gmail.com>
2017-10-04 08:05:48 +0900

    ちょっとメッセージ変えた

---
+++ app.py
@@ -0,0 +1,36 @@
+import tornado.ioloop
+import tornado.web
+from jinja2 import Environment, FileSystemLoader
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        env = Environment(loader=FileSystemLoader('./', encoding='utf8'))
+        template = env.get_template('./login.html')
+        self.write(template.render())
+
+
+class RegisterHandler(tornado.web.RequestHandler):
+    def post(self):
+        # 登録処理
+        self.redirect(r"/thanks")
+
+
+class ThanksHandler(tornado.web.RequestHandler):
+    def get(self):
+        # 登録処理
+        self.write("Thanks!!! You are redirected!!")
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/register", RegisterHandler),
+        (r"/thanks", ThanksHandler),
+    ])
+
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
