8eb16b5f9c321b550c6d4f650f05e6371b4a8def
Hiroaki Ogasawara <hiroga1030@gmail.com>
2018-06-15 00:42:45 +0900

    CryptZombieもくもく会での進捗をコミット

--- blockchain/eth/solidity/Contract.sol
+++ blockchain/eth/solidity/Contract.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.4.19;
 
 contract ZombieFactory {
 
+    event NewZombie(uint zombieId, string name, uint dna);
+    // contractのフィールドに定義し、functionで発火させ、web3.jsでlistenする。
+
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
     // uint... unsigned integerのこと（符号なし整数）
@@ -15,15 +18,28 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function createRandomZombie(string _name) public {
+        require(ownerZombieCount[msg.sender] == 0); // if文とthorwの合わせ技的な文法。
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
     // private関数は_で始めるのが通例
     function _createZombie(string _name, uint _dna) private {
-        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        NewZombie(id, _name, _dna);
     }
 
     // return ではなく returns
     // アプリケーション内のデータを更新できない関数はviewを、参照さえできない関数はpureを宣言する。
-    function _generateRandomDna() private view returns(uint){
-        
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
     }
 
 }
