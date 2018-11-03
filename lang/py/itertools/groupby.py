b2bc1359b6e82f2fef5419b314b12569150572fb
Hiroaki Ogasawara <hiroga1030@gmail.com>
2018-11-03 20:13:49 +0900

    iteration

--- lang/py/itertools/groupby.py
+++ lang/py/itertools/groupby.py
@@ -15,7 +15,7 @@ for i, grouped_items in groupby(enumerate(some_list), lambda tuple: tuple[0]//2)
     print(i, [item for item in grouped_items])
 """
 0 [(0, 'a'), (1, 'b')]
-1 [(2, 'c'), (3, 'd')]
+1 [(2, 'c'), (3, 'd')]  
 2 [(4, 'e')]
 """
 
