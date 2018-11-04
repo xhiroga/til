a97b4f1d44f1e1ff156d514a182c34619fb18257
Hiroaki Ogasawara <hiroga1030@gmail.com>
2018-11-04 10:59:09 +0900

    cfnの実験に使ったスクリプト。便利

---
+++ cloud/aws/cfn/scripts/cfn.sh
@@ -0,0 +1,16 @@
+cmd=$1
+stack_name=$2
+
+if [ $cmd = "create" ]; then
+    case $3 in
+        "") echo "Error: Please give a template file path!"; exit 0;;
+        /*) absolute=$3 ;;
+        *) absolute=$PWD/$3 ;;
+        # S3にはこれから対応する、かも
+    esac
+    aws cloudformation create-stack --stack-name $stack_name --template-body file://$absolute
+elif [ $cmd = "delete" ]; then
+    aws cloudformation delete-stack --stack-name $stack_name
+elif [ $cmd = "status" ]; then
+    aws cloudformation describe-stacks --stack-name $stack_name | jq .Stacks[] | jq '.StackId, .StackStatus'
+fi
