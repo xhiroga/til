d4e5053619b92922212a162bc10784d19c00ba13
Hiroaki Ogasawara <hiroga1030@gmail.com>
2019-01-23 09:17:39 +0900

    🐔 < First commit!

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+package blog
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("My Example Blog", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+}
+
+// See: https://ktor.io/quickstart/quickstart/gradle.html
+
+// TODO: Separate applicatoin module from main function
+// TODO: extract configuration
