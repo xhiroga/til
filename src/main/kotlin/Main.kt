9c10c4ab319214d4cadacc4102164f41912377f9
Hiroaki Ogasawara <hiroga1030@gmail.com>
2019-01-23 10:23:20 +0900

    new endpoint /tasks. return json is not correct but works

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
-package blog
+package todolist
+
+// MEMO: 自作クラスがimportできなかったら、それはpackageが同じかどうかを疑うといいかも
 
 import io.ktor.application.*
 import io.ktor.http.*
@@ -6,12 +8,34 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import java.lang.Exception
 
 fun main(args: Array<String>) {
     embeddedServer(Netty, 8080) {
         routing {
             get("/") {
                 call.respondText("My Example Blog", ContentType.Text.Html)
+            }
+            get("/tasks") {
+                try {
+                    println(call.request)
+
+//               TODO: なぜかObjectMapper()のインスタンスを作るとレスポンスが返らなくなる。
+//               val objectMapper = ObjectMapper().registerKotlinModule()
+
+                    val tasks = listOf(
+                        Task(1, "みかん販売の看板を作成する", false),
+                        Task(2, "トイレットペーパーを購入する", true)
+                    )
+                    // call.respondがtransformしてくれるのはdata class単体に限るらしい？
+                    call.respondText(
+                        tasks.toString()
+                    )
+                } catch (error: Exception) {
+                    println(error)
+                }
             }
         }
     }.start(wait = true)
