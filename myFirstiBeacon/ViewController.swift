1964f1e1c41b32a2fe62f29920430e5bd19f1995
Hiroaki Ogasawara <hiroga1030@gmail.com>
2019-10-26 15:55:31 +0900

    Initial Commit

---
+++ myFirstiBeacon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myFirstiBeacon
+//
+//  Created by Hiroaki Ogasawara on 2019/10/26.
+//  Copyright © 2019 Hiroaki Ogasawara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
