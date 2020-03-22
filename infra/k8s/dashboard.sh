8c5464b66468abe2fd42c54d817f0fb87384ea3f
Hiroaki Ogasawara <hiroga1030@gmail.com>
2020-03-22 16:14:13 +0900

    open dashboard for loal

--- infra/k8s/dashboard.sh
+++ infra/k8s/dashboard.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta8/aio/deploy/recommended.yaml
-# kubectl proxy
+kubectl proxy &
+kubectl -n kubernetes-dashboard describe secrets default | grep 'token:' | awk '{ print $2 }'
 open http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
