b14a7c6dc8a3a40b1cd2908d9daa78dcbad12374
Hiroaki Ogasawara <hiroga1030@gmail.com>
2020-08-30 23:39:54 +0900

    can talk throug video

--- web/webrtc/introduction-to-webrtc/server.js
+++ web/webrtc/introduction-to-webrtc/server.js
@@ -5,8 +5,10 @@ const io = require("socket.io")(http);
 
 io.on("connection", (socket) => {
   socket.on("ready", (req) => {
-    const room = req;
+    const room = req.chat_room;
+    const signal_room = req.signal_room;
     socket.join(room);
+    socket.join(signal_room);
     socket.broadcast.emit("announce", {
       message: `New Client in the room: ${room}`,
     });
@@ -14,9 +16,16 @@ io.on("connection", (socket) => {
   socket.on("send", (req) => {
     console.log(req);
     socket.join(req.room);
+    // if use socket.emit(), message is emitted to all client including sender.
     socket.broadcast.emit("message", {
       message: req.message,
       author: req.author,
+    });
+  });
+  socket.on("signal", (req) => {
+    socket.broadcast.emit("signaling_message", {
+      type: req.type,
+      message: req.message,
     });
   });
 });
