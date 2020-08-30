d3e10b98e75977ec743c30dfbd7dc60bb9bd1f3c
Hiroaki Ogasawara <hiroga1030@gmail.com>
2020-08-30 20:59:16 +0900

    use socket.io

--- web/webrtc/intro-webrtc/server.js
+++ web/webrtc/intro-webrtc/server.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-console.log("server started");
+io.on("connection", (socket) => {
+  socket.on("ready", (req) => {
+    const room = req;
+    socket.join(room);
+    socket.broadcast.emit("announce", {
+      message: `New Client in the room: ${room}`,
+    });
+  });
+  socket.on("send", (req) => {
+    console.log(req);
+    socket.join(req.room);
+    socket.broadcast.emit("message", {
+      message: req.message,
+      author: req.author,
+    });
+  });
+});
 
 app.use(express.static(__dirname + "/public"));
 
@@ -9,4 +27,7 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
-app.listen(3000);
+PORT = 3000;
+http.listen(PORT, () => {
+  console.log("server started on port " + PORT);
+});
