8adbdbb261d9ebf608345768e071409517569d26
Hiroaki Ogasawara <hiroga1030@gmail.com>
2020-10-09 14:25:26 +0900

    work correctly

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,5 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -13,11 +9,36 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('cdk-python-dotvscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('cdk-python-dotvscode.dotvscode-dir-and-setting-json', async () => {
+		const getWorkspaceDirPath = () => {
+			if (vscode.workspace.workspaceFolders !== undefined){
+				return vscode.workspace.workspaceFolders[0].uri
+			}
+			return undefined
+		}
+		
+		const isCdkProject = async (workspaceDirPath: vscode.Uri) => {
+			return await vscode.workspace.fs.stat(vscode.Uri.file(`${workspaceDirPath.toString().split(':')[1]}/cdk.out`)).then( stat =>{
+				return stat.type === vscode.FileType.Directory
+			})
+		}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from cdk-python-dotvscode!');
+		const setConfig  = async (workspaceDirPath: vscode.Uri) => {
+			const config = vscode.workspace.getConfiguration('python', workspaceDirPath)
+			// to specify where libraries such as aws-cdk were installed.
+			config.update("pythonPath", "${workspaceFolder}/.env/bin/python", false)
+			config.update("envFile", "${workspaceFolder}/.env", false)
+		}
+
+		const workspaceDirPath  = getWorkspaceDirPath()
+		if (workspaceDirPath !== undefined && isCdkProject(workspaceDirPath)){
+			setConfig(workspaceDirPath)
+			vscode.window.showInformationMessage(
+				".vscode and setting.json was created"
+			)
+		}else {
+			// TODO
+		}
 	});
 
 	context.subscriptions.push(disposable);
