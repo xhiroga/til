24b7229633873c9790638a8e31210b26c5eb4ac4
xhiroga <hiroga@justincase-tech.com>
2020-10-30 08:21:02 +0900

    feat: hello world

--- lang/kt/ktor/ktor-nuxtjs-webapp-introduction/todo-api/src/Application.kt
+++ lang/kt/ktor/ktor-nuxtjs-webapp-introduction/todo-api/src/Application.kt
@@ -1,13 +1,21 @@
 package com.todo.example
 
 import io.ktor.application.*
+import io.ktor.http.ContentType
 import io.ktor.response.*
 import io.ktor.request.*
+import io.ktor.routing.get
+import io.ktor.routing.routing
 
 fun main(args: Array<String>): Unit = io.ktor.server.tomcat.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+    routing{
+        get("/"){
+            call.respondText("Hello, world!", ContentType.Text.Plain)
+        }
+    }
 }
 
