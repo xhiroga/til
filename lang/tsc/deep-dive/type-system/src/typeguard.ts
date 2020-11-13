30ae7ea47c62b2665fa9463de6b6fe0442b279cb
Hiroaki Ogasawara <hiroga@hiroga-jict-MBP.local>
2020-11-13 10:07:07 +0900

    learn about moving types.

--- lang/tsc/deep-dive/type-system/src/typeguard.ts
+++ lang/tsc/deep-dive/type-system/src/typeguard.ts
@@ -35,3 +35,26 @@ function doStuff(arg: Foo | Bar) {
 
 doStuff({ foo: 123, common: '123' });
 doStuff({ bar: 123, common: '123' });
+
+// in演算子を使えばプロパティの有無で型ガードが使える。更に、同名プロパティで型がリテラル型の場合、それも判別に使える。
+interface Groudon {
+    kind: "ground",
+    earthQuake: 100
+}
+
+interface Kyogre {
+    kind: "water",
+    surf: 90
+}
+
+type HoennLegend = Groudon | Kyogre
+
+function goHoennLegend(legend: HoennLegend) {
+    if (legend.kind === "ground") {
+        console.log(legend.earthQuake)
+    } else if (legend.kind === "water") {
+        console.log(legend.surf)
+    } else {
+        const _exhaustiveCheck: never = legend  // これまでの私は単にエラーを投げていたが、この書き方（想定外のケースではコンパイルエラーにする）だとコンパイル時に気付ける。
+    }
+}
