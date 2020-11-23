be941621133ce339e362e71a9a03c0a0e8a70a01
hiroga <hiroga1030@gmail.com>
2020-11-23 10:04:18 +0900

    get by xpath

--- lang/py/scrapy/web-scraping-in-python-using-scrapy-and-splash/README.md
+++ lang/py/scrapy/web-scraping-in-python-using-scrapy-and-splash/README.md
@@ -1,3 +1,13 @@
 # Modern Web Scraping with Python using Scrapy Splash Selenium
 
 https://www.udemy.com/course/web-scraping-in-python-using-scrapy-and-splash/learn/lecture/11497662#overview
+
+## Scrapy Shell
+
+```terminal
+scrapy shell
+>>> r = scrapy.Request("https://www.worldometers.info/world-population/population-by-country/")
+>>> fetch(r)
+>>> response.body   # fetch()のMutableなオブジェクト
+>>> view(response)
+```
--- lang/py/scrapy/web-scraping-in-python-using-scrapy-and-splash/worldmaters/worldmaters/spiders/countries.py
+++ lang/py/scrapy/web-scraping-in-python-using-scrapy-and-splash/worldmaters/worldmaters/spiders/countries.py
@@ -4,7 +4,13 @@ import scrapy
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
     allowed_domains = ['www.worldmeters.info/']     # only domain. no protocol, no path
-    start_urls = ['https://www.worldometers.info/world-population/poplulation-by-country/']  # fix to https
+    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']  # fix to https
 
     def parse(self, response):
-        pass
+        title = response.xpath("//h1/text()").get()
+        countries = response.xpath("//td/a/text()").getall()
+
+        yield{
+            'title': title,
+            'countries': countries
+        }
