3f981f12d60c626acd8299e8f4026ced78f607bb
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2021-05-03 12:32:13 +0900

    kubernetes: modeling pod, replicaset and deployment

--- infra/k8s/models/model-ts/models.ts
+++ infra/k8s/models/model-ts/models.ts
@@ -1,11 +1,55 @@
+export type Kind = "Pod" | "PodList" | "ReplicaSet" | "ReplicaSetList";
+export type ApiVersion = "v1" | "apps/v1";
+
 export type ApiResponse = {
-  kind: string;
-  apiVersion: string;
+  kind: Kind;
+  apiVersion: ApiVersion;
+};
+export type ApiListResponse = ApiResponse & {
   metadata: {
-    selflink?: string;
     resourceVersion: string;
   };
-  items: NameSpace[] | Node[];
+  items: [];
+};
+
+export type Container = {
+  image: string;
+  name: string;
+  ports: [];
+};
+
+export type Pod = {
+  metadata: any;
+  spec: {
+    containers: Container[];
+  };
+  storageVolume: any;
+};
+export type PodResponse = ApiResponse & Pod;
+export type PodListResponse = ApiListResponse & {
+  items: Pod[];
+};
+
+export type ReplicaSet = {
+  metadata: any;
+  spec: {
+    replicas: number;
+    selector: any;
+    template: Pod;
+  };
+  status: any;
+};
+export type ReplicaSetResponse = ApiResponse & ReplicaSet;
+export type ReplicaSetListResponse = ApiListResponse & {
+  items: ReplicaSet[];
+};
+
+export type Deployment = {
+  strategy: any;
+} & ReplicaSet;
+export type DeploymentResponse = ApiResponse & Deployment;
+export type DeploymentListResponse = ApiListResponse & {
+  items: Deployment[];
 };
 
 export type Network = {};
@@ -35,11 +79,4 @@ export type NodeMetadata = {
 
 export type Worker = {};
 
-export type Pod = {
-  container: any;
-  storageVolume: any;
-};
-
 export type Service = {};
-
-export type Deployment = {};
