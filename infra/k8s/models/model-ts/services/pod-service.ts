060c6927f4bf95efaf2269d5834b3e20e850c422
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2021-05-09 13:39:24 +0900

    can get namespace

--- infra/k8s/models/model-ts/services/pod-service.ts
+++ infra/k8s/models/model-ts/services/pod-service.ts
@@ -1,4 +1,12 @@
-const getPods = () => {};
+import { podMap } from "../db/reources-map.ts";
+
+const getPods = (namespace?: string) => {
+  if (namespace === undefined) {
+    return podMap.flatMap((map) => map.pods);
+  } else {
+    return podMap.filter((map) => map.namespace === namespace)[0].pods;
+  }
+};
 const createPod = (any: any) => {
   console.log(any);
 };
