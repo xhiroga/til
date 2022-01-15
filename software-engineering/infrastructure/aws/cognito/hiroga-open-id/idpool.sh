5bdb6910bc601ecc9b1ac15d729a6c089162f93e
xhiroga <13391129+xhiroga@users.noreply.github.com>
2022-01-15 10:45:19 +0900

    chore: no just shell

--- software-engineering/infrastructure/aws/cognito/hiroga-open-id/idpool.sh
+++
@@ -1,9 +0,0 @@
-aws cognito-identity create-identity-pool --identity-pool-name myId --allow-unauthenticated-identities
-
-````
-{
-    "IdentityPoolId": "XXXXXXXXXX",
-    "IdentityPoolName": "myId",
-    "AllowUnauthenticatedIdentities": true
-}
-```
