d7b3412b5446d7fb649479d9ac4749ab4e224e81
Hiroaki Ogasawara <hiroga1030@gmail.com>
2022-06-19 20:38:44 +0900

    feat: ball bound

--- software-engineering/programming-languages/html/canvas-api-advanced-animations/index.js
+++ software-engineering/programming-languages/html/canvas-api-advanced-animations/index.js
@@ -1,10 +1,15 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+var raf;
 
-var ball = {
+const radius = 25;
+
+const ball = {
   x: 100,
   y: 100,
-  radius: 25,
+  vx: 5,
+  vy: 2,
+  radius,
   color: "blue",
   draw: function () {
     ctx.beginPath();
@@ -14,5 +19,35 @@ var ball = {
     ctx.fill();
   },
 };
+
+const draw = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ball.draw();
+  if (
+    ball.y + ball.vy + radius > canvas.height ||
+    ball.y + ball.vy - radius < 0
+  ) {
+    ball.vy = -ball.vy;
+  } else {
+    ball.y += ball.vy;
+  }
+  if (
+    ball.x + ball.vx + radius > canvas.width ||
+    ball.x + ball.vx - radius < 0
+  ) {
+    ball.vx = -ball.vx;
+  } else {
+    ball.x += ball.vx;
+  }
+  raf = window.requestAnimationFrame(draw);
+};
+
+canvas.addEventListener("mouseover", function (e) {
+  raf = window.requestAnimationFrame(draw);
+});
+
+canvas.addEventListener("mouseout", function (e) {
+  window.cancelAnimationFrame(raf);
+});
 
 ball.draw();
