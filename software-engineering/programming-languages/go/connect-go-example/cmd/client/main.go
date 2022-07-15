c78f2b9740fde6499ce3eeb036f524ba641d86aa
Hiroaki Ogasawara <hiroga1030@gmail.com>
2022-07-15 18:19:53 +0900

    feat: protbuf client and server

---
+++ software-engineering/programming-languages/go/connect-go-example/cmd/client/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	greetv1 "example/gen/greet/v1"
+	"example/gen/greet/v1/greetv1connect"
+
+	"github.com/bufbuild/connect-go"
+)
+
+func main() {
+	client := greetv1connect.NewGreetServiceClient(
+		http.DefaultClient,
+		"http://localhost:8080",
+		connect.WithGRPC(),
+	)
+	res, err := client.Greet(
+		context.Background(),
+		connect.NewRequest(&greetv1.GreetRequest{Name: "Jane"}),
+	)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(res.Msg.Greeting)
+}
