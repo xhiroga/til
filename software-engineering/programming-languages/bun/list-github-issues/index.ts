8877f46b9cac763b4416aee0e38637aae4927a73
xhiroga <13391129+xhiroga@users.noreply.github.com>
2022-07-21 13:52:11 +0900

    feat: can output csv

--- software-engineering/programming-languages/bun/list-github-issues/index.ts
+++ software-engineering/programming-languages/bun/list-github-issues/index.ts
@@ -1,11 +1,24 @@
 import { Octokit } from "@octokit/core";
+import { restEndpointMethods } from "@octokit/plugin-rest-endpoint-methods";
+import { getJukoushaStartupPhase, getSessionAbstract, getSessionFormat, getSessionHosoku, getSessionTopic, getToudanshaStartupPhase, getTwitterId } from "./util";
 
-const octokit = new Octokit();
+const MyOctotKit = Octokit.plugin(restEndpointMethods);
+const octokit = new MyOctotKit()
 
-const response = await octokit.request("GET /repos/{org}/{repo}/issues", {
-  org: "aws-startup-community",
-  repo: "aws-startup-community-conference-2022-cfp",
-  type: "private",
-});
+const response = await octokit.rest.issues.listForRepo({ owner: "aws-startup-community", repo: "aws-startup-community-conference-2022-cfp", state: "open" })
 
-console.log(response)
+const csv = response.data.map(({ title, body }) => {
+    if (!body) {
+        return
+    }
+    const twitterId = getTwitterId(body)
+    const sessionAbstract = getSessionAbstract(body)
+    const sessionHosoku = getSessionHosoku(body)
+    const toudanshaStartupPhase = getToudanshaStartupPhase(body)
+    const jukoushaStartupPhase = getJukoushaStartupPhase(body)
+    const sessionTopic = getSessionTopic(body)
+    const sessionFormat = getSessionFormat(body)
+    return `${title},${twitterId},${sessionAbstract},${sessionHosoku},${toudanshaStartupPhase},${jukoushaStartupPhase},${sessionTopic},${sessionFormat}`
+}).join("\n")
+console.log("タイトル,Twitter ID,アブストラクト,補足,所属Startupフェーズ,受講者スタートアップフェーズ,トピック,フォーマット")
+console.log(csv)
