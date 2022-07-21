7a37ffa7155cbfaac270d22c738de7cb09f8cfee
xhiroga <13391129+xhiroga@users.noreply.github.com>
2022-07-21 13:52:11 +0900

    feat: can output csv

---
+++ software-engineering/programming-languages/bun/list-github-issues/util.ts
@@ -0,0 +1,36 @@
+export const getTwitterId = (body: string) => {
+    // なぜか () がマッチしない。bunのランタイムには正規表現のエスケープに関するバグがあるのだろうか？
+    const res = body.match(/### Twitter ID .+\n\n(\w{1,15})\n\n/)
+    return res ? res[1] : null
+}
+
+export const getSessionAbstract = (body: string) => {
+    // カタカナもマッチしないようだ...
+    const res = body.match(/### .+250.+\n\n(.*)\n\n###/)
+    return res ? res[1] : null
+}
+
+export const getSessionHosoku = (body: string) => {
+    const res = body.match(/### .+800.+\n\n(.*)\n\n###/)
+    return res ? res[1] : null
+}
+
+export const getToudanshaStartupPhase = (body: string) => {
+    const res = body.match(/### .+800.+\n\n.*\n\n### .+\n\n(.*)\n\n###/)
+    return res ? res[1] : null
+}
+
+export const getJukoushaStartupPhase = (body: string) => {
+    const res = body.match(/### .+800.+\n\n.*\n\n### .+\n\n.*\n\n### .+\n\n(.*)\n\n/)
+    return res ? res[1] : null
+}
+
+export const getSessionTopic = (body: string) => {
+    const res = body.match(/### .+800.+\n\n.*\n\n### .+\n\n.*\n\n### .+\n\n.*\n\n### .+\n\n(.*)\n\n/)
+    return res ? res[1] : null
+}
+
+export const getSessionFormat = (body: string) => {
+    const res = body.match(/### .+\n\n(.*)$/)
+    return res ? res[1] : null
+}
