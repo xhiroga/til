93bff18e2cdc695dc5f87194e2b5d449ff57d769
xhiroga <13391129+xhiroga@users.noreply.github.com>
2022-08-13 19:46:43 +0900

    feat: can debug

--- software-engineering/infrastructure/aws/cost-explorer/aws-get-cost-super-groupby/aws/ce/ce.go
+++ software-engineering/infrastructure/aws/cost-explorer/aws-get-cost-super-groupby/aws/ce/ce.go
@@ -1,27 +1,28 @@
 package ce
 
 import (
-	"fmt"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/costexplorer"
 )
 
-func Get() {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{}))
-	svc := costexplorer.New(
-		sess,
-	)
-	res, err := svc.GetCostAndUsage(
-		&costexplorer.GetCostAndUsageInput{
-			Granularity: aws.String("MONTHLY"),
-			GroupBy:     []*costexplorer.GroupDefinition{{Key: aws.String("SERVICE"), Type: aws.String("DIMENSION")}},
-			Metrics:     []*string{aws.String("BlendedCost")},
-			// NextPageToken: new(string),
-			TimePeriod: &costexplorer.DateInterval{Start: aws.String("2022-01-01"), End: aws.String("2022-08-31")},
-		},
-	)
-	fmt.Println(res)
-	fmt.Errorf("%v", err)
+type RetinaCostExplorer struct {
+	*costexplorer.CostExplorer
 }
+
+func NewRetinaCostExplorer(ce *costexplorer.CostExplorer) *RetinaCostExplorer {
+	return &RetinaCostExplorer{
+		CostExplorer: ce,
+	}
+}
+
+func (svc *RetinaCostExplorer) GetRetinaCostAndUsage(input *costexplorer.GetCostAndUsageInput) (*costexplorer.GetCostAndUsageOutput, error) {
+	return svc.GetCostAndUsage(input)
+}
+
+// func TestGetRetinaCostAndUsage(t *testing.T) {
+// 	tests := []struct {
+// 		name    string
+// 		args    *costexplorer.GetCostAndUsageInput
+// 		want    *costexplorer.GetCostAndUsageOutput
+// 		wantErr bool
+// 	}{}
+// }
