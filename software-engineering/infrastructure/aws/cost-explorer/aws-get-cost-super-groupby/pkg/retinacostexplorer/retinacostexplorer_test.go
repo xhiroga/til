e4b2b649553aaffa7942118610da205fda0ef557
xhiroga <13391129+xhiroga@users.noreply.github.com>
2022-08-13 22:54:14 +0900

    test(retina-ce): pass normal case

--- software-engineering/infrastructure/aws/cost-explorer/aws-get-cost-super-groupby/pkg/retinacostexplorer/retinacostexplorer_test.go
+++ software-engineering/infrastructure/aws/cost-explorer/aws-get-cost-super-groupby/pkg/retinacostexplorer/retinacostexplorer_test.go
@@ -12,95 +12,93 @@ import (
 )
 
 func TestGetRetinaCostAndUsage(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-
-	input1 := &costexplorer.GetCostAndUsageInput{
-		Granularity: aws.String("MONTHLY"),
-		GroupBy: []*costexplorer.GroupDefinition{
-			{
-				Type: aws.String("DIMENSION"),
-				Key:  aws.String("LINKED_ACCOUNT"),
-			}, {
-				Type: aws.String("DIMENSION"),
-				Key:  aws.String("SERVICE"),
-			},
-		},
-		Metrics: []*string{aws.String("BlendedCost")},
-		TimePeriod: &costexplorer.DateInterval{
-			Start: aws.String("2022-01-01"),
-			End:   aws.String("2022-01-31"),
-		},
-	}
-	output1 := &costexplorer.GetCostAndUsageOutput{
-		DimensionValueAttributes: []*costexplorer.DimensionValuesWithAttributes{
-			{
-				Attributes: map[string]*string{
-					"description": aws.String("dev"),
-				},
-				Value: aws.String("123456789012"),
-			}, {
-				Attributes: map[string]*string{
-					"description": aws.String("prod"),
-				},
-				Value: aws.String("987654321098"),
-			},
-		},
-		GroupDefinitions: []*costexplorer.GroupDefinition{{
-			Key:  aws.String("LINKED_ACCOUNT"),
-			Type: aws.String("DIMENSION"),
-		}, {
-			Key:  aws.String("USAGE_TYPE"),
-			Type: aws.String("DIMENSION"),
-		}},
-		ResultsByTime: []*costexplorer.ResultByTime{{
-			Estimated: aws.Bool(false),
-			Groups: []*costexplorer.Group{
-				{
-					Keys: []*string{aws.String("123456789012"), aws.String("EC2 - Other")},
-					Metrics: map[string]*costexplorer.MetricValue{
-						"BlendedCost": {
-							Amount: aws.String("3.716129016"),
-							Unit:   aws.String("USD"),
-						},
-					},
-				}, {
-					Keys: []*string{aws.String("123456789012"), aws.String("AWS CloudTrail")},
-					Metrics: map[string]*costexplorer.MetricValue{
-						"BlendedCost": {
-							Amount: aws.String("0"),
-							Unit:   aws.String("USD"),
-						},
-					},
-				}, {
-					Keys: []*string{aws.String("987654321098"), aws.String("AWS CloudTrail")},
-					Metrics: map[string]*costexplorer.MetricValue{
-						"BlendedCost": {
-							Amount: aws.String("2.90684"),
-							Unit:   aws.String("USD"),
-						},
-					},
-				},
-			},
-			TimePeriod: &costexplorer.DateInterval{
-				End:   aws.String("2022-01-31"),
-				Start: aws.String("2022-01-01"),
-			},
-			Total: map[string]*costexplorer.MetricValue{},
-		}},
-	}
-
-	mockCostExplorer := mock_retinacostexplorer.NewMockCostExplorer(mockCtrl)
-	mockCostExplorer.EXPECT().GetCostAndUsage(input1).Return(output1, nil)
 
 	tests := []struct {
-		name    string
-		args    *costexplorer.GetCostAndUsageInput
-		want    *costexplorer.GetCostAndUsageOutput
-		wantErr bool
+		name          string
+		prepareMockFn func(mockCostExplorer *mock_retinacostexplorer.MockCostExplorer)
+		args          *costexplorer.GetCostAndUsageInput
+		want          *costexplorer.GetCostAndUsageOutput
+		wantErr       bool
 	}{
 		{
-			name: "success",
+			name: "normal (2 groups)",
+			prepareMockFn: func(mockCostExplorer *mock_retinacostexplorer.MockCostExplorer) {
+				input1 := &costexplorer.GetCostAndUsageInput{
+					Granularity: aws.String("MONTHLY"),
+					GroupBy: []*costexplorer.GroupDefinition{
+						{
+							Type: aws.String("DIMENSION"),
+							Key:  aws.String("LINKED_ACCOUNT"),
+						}, {
+							Type: aws.String("DIMENSION"),
+							Key:  aws.String("SERVICE"),
+						},
+					},
+					Metrics: []*string{aws.String("BlendedCost")},
+					TimePeriod: &costexplorer.DateInterval{
+						Start: aws.String("2022-01-01"),
+						End:   aws.String("2022-01-31"),
+					},
+				}
+				output1 := &costexplorer.GetCostAndUsageOutput{
+					DimensionValueAttributes: []*costexplorer.DimensionValuesWithAttributes{
+						{
+							Attributes: map[string]*string{
+								"description": aws.String("dev"),
+							},
+							Value: aws.String("123456789012"),
+						}, {
+							Attributes: map[string]*string{
+								"description": aws.String("prod"),
+							},
+							Value: aws.String("987654321098"),
+						},
+					},
+					GroupDefinitions: []*costexplorer.GroupDefinition{{
+						Type: aws.String("DIMENSION"),
+						Key:  aws.String("LINKED_ACCOUNT"),
+					}, {
+						Type: aws.String("DIMENSION"),
+						Key:  aws.String("USAGE_TYPE"),
+					}},
+					ResultsByTime: []*costexplorer.ResultByTime{{
+						Estimated: aws.Bool(false),
+						Groups: []*costexplorer.Group{
+							{
+								Keys: []*string{aws.String("123456789012"), aws.String("EC2 - Other")},
+								Metrics: map[string]*costexplorer.MetricValue{
+									"BlendedCost": {
+										Amount: aws.String("3.716129016"),
+										Unit:   aws.String("USD"),
+									},
+								},
+							}, {
+								Keys: []*string{aws.String("123456789012"), aws.String("AWS CloudTrail")},
+								Metrics: map[string]*costexplorer.MetricValue{
+									"BlendedCost": {
+										Amount: aws.String("0"),
+										Unit:   aws.String("USD"),
+									},
+								},
+							}, {
+								Keys: []*string{aws.String("987654321098"), aws.String("AWS CloudTrail")},
+								Metrics: map[string]*costexplorer.MetricValue{
+									"BlendedCost": {
+										Amount: aws.String("2.90684"),
+										Unit:   aws.String("USD"),
+									},
+								},
+							},
+						},
+						TimePeriod: &costexplorer.DateInterval{
+							End:   aws.String("2022-01-31"),
+							Start: aws.String("2022-01-01"),
+						},
+						Total: map[string]*costexplorer.MetricValue{},
+					}},
+				}
+				mockCostExplorer.EXPECT().GetCostAndUsage(input1).Return(output1, nil)
+			},
 			args: &costexplorer.GetCostAndUsageInput{
 				Granularity: aws.String("MONTHLY"),
 				GroupBy: []*costexplorer.GroupDefinition{
@@ -110,9 +108,6 @@ func TestGetRetinaCostAndUsage(t *testing.T) {
 					}, {
 						Type: aws.String("DIMENSION"),
 						Key:  aws.String("SERVICE"),
-					}, {
-						Type: aws.String("DIMENSION"),
-						Key:  aws.String("USAGE_TYPE"),
 					},
 				},
 				Metrics: []*string{aws.String("BlendedCost")},
@@ -140,16 +135,13 @@ func TestGetRetinaCostAndUsage(t *testing.T) {
 					Key:  aws.String("LINKED_ACCOUNT"),
 				}, {
 					Type: aws.String("DIMENSION"),
-					Key:  aws.String("SERVICE"),
-				}, {
-					Type: aws.String("DIMENSION"),
 					Key:  aws.String("USAGE_TYPE"),
 				}},
 				ResultsByTime: []*costexplorer.ResultByTime{{
 					Estimated: aws.Bool(false),
 					Groups: []*costexplorer.Group{
 						{
-							Keys: []*string{aws.String("123456789012"), aws.String("EC2 - Other"), aws.String("APN1-EBS:VolumeUsage.gp2")},
+							Keys: []*string{aws.String("123456789012"), aws.String("EC2 - Other")},
 							Metrics: map[string]*costexplorer.MetricValue{
 								"BlendedCost": {
 									Amount: aws.String("3.716129016"),
@@ -157,18 +149,18 @@ func TestGetRetinaCostAndUsage(t *testing.T) {
 								},
 							},
 						}, {
-							Keys: []*string{aws.String("987654321098"), aws.String("AWS CloudTrail"), aws.String("APN1-PaidEventsRecorded")},
+							Keys: []*string{aws.String("123456789012"), aws.String("AWS CloudTrail")},
 							Metrics: map[string]*costexplorer.MetricValue{
 								"BlendedCost": {
-									Amount: aws.String("1.37946"),
+									Amount: aws.String("0"),
 									Unit:   aws.String("USD"),
 								},
 							},
 						}, {
-							Keys: []*string{aws.String("987654321098"), aws.String("AWS CloudTrail"), aws.String("USE1-PaidEventsRecorded")},
+							Keys: []*string{aws.String("987654321098"), aws.String("AWS CloudTrail")},
 							Metrics: map[string]*costexplorer.MetricValue{
 								"BlendedCost": {
-									Amount: aws.String("1.52738"),
+									Amount: aws.String("2.90684"),
 									Unit:   aws.String("USD"),
 								},
 							},
@@ -183,10 +175,177 @@ func TestGetRetinaCostAndUsage(t *testing.T) {
 			},
 			wantErr: false,
 		},
+		// {
+		// 	name: "3 groups",
+		// 	prepareMockFn: func(mockCostExplorer *mock_retinacostexplorer.MockCostExplorer) {
+		// 		input1 := &costexplorer.GetCostAndUsageInput{
+		// 			Granularity: aws.String("MONTHLY"),
+		// 			GroupBy: []*costexplorer.GroupDefinition{
+		// 				{
+		// 					Type: aws.String("DIMENSION"),
+		// 					Key:  aws.String("LINKED_ACCOUNT"),
+		// 				}, {
+		// 					Type: aws.String("DIMENSION"),
+		// 					Key:  aws.String("SERVICE"),
+		// 				},
+		// 			},
+		// 			Metrics: []*string{aws.String("BlendedCost")},
+		// 			TimePeriod: &costexplorer.DateInterval{
+		// 				Start: aws.String("2022-01-01"),
+		// 				End:   aws.String("2022-01-31"),
+		// 			},
+		// 		}
+		// 		output1 := &costexplorer.GetCostAndUsageOutput{
+		// 			DimensionValueAttributes: []*costexplorer.DimensionValuesWithAttributes{
+		// 				{
+		// 					Attributes: map[string]*string{
+		// 						"description": aws.String("dev"),
+		// 					},
+		// 					Value: aws.String("123456789012"),
+		// 				}, {
+		// 					Attributes: map[string]*string{
+		// 						"description": aws.String("prod"),
+		// 					},
+		// 					Value: aws.String("987654321098"),
+		// 				},
+		// 			},
+		// 			GroupDefinitions: []*costexplorer.GroupDefinition{{
+		// 				Type: aws.String("DIMENSION"),
+		// 				Key:  aws.String("LINKED_ACCOUNT"),
+		// 			}, {
+		// 				Type: aws.String("DIMENSION"),
+		// 				Key:  aws.String("USAGE_TYPE"),
+		// 			}},
+		// 			ResultsByTime: []*costexplorer.ResultByTime{{
+		// 				Estimated: aws.Bool(false),
+		// 				Groups: []*costexplorer.Group{
+		// 					{
+		// 						Keys: []*string{aws.String("123456789012"), aws.String("EC2 - Other")},
+		// 						Metrics: map[string]*costexplorer.MetricValue{
+		// 							"BlendedCost": {
+		// 								Amount: aws.String("3.716129016"),
+		// 								Unit:   aws.String("USD"),
+		// 							},
+		// 						},
+		// 					}, {
+		// 						Keys: []*string{aws.String("123456789012"), aws.String("AWS CloudTrail")},
+		// 						Metrics: map[string]*costexplorer.MetricValue{
+		// 							"BlendedCost": {
+		// 								Amount: aws.String("0"),
+		// 								Unit:   aws.String("USD"),
+		// 							},
+		// 						},
+		// 					}, {
+		// 						Keys: []*string{aws.String("987654321098"), aws.String("AWS CloudTrail")},
+		// 						Metrics: map[string]*costexplorer.MetricValue{
+		// 							"BlendedCost": {
+		// 								Amount: aws.String("2.90684"),
+		// 								Unit:   aws.String("USD"),
+		// 							},
+		// 						},
+		// 					},
+		// 				},
+		// 				TimePeriod: &costexplorer.DateInterval{
+		// 					End:   aws.String("2022-01-31"),
+		// 					Start: aws.String("2022-01-01"),
+		// 				},
+		// 				Total: map[string]*costexplorer.MetricValue{},
+		// 			}},
+		// 		}
+		// 		mockCostExplorer.EXPECT().GetCostAndUsage(input1).Return(output1, nil)
+		// 	},
+		// 	args: &costexplorer.GetCostAndUsageInput{
+		// 		Granularity: aws.String("MONTHLY"),
+		// 		GroupBy: []*costexplorer.GroupDefinition{
+		// 			{
+		// 				Type: aws.String("DIMENSION"),
+		// 				Key:  aws.String("LINKED_ACCOUNT"),
+		// 			}, {
+		// 				Type: aws.String("DIMENSION"),
+		// 				Key:  aws.String("SERVICE"),
+		// 			}, {
+		// 				Type: aws.String("DIMENSION"),
+		// 				Key:  aws.String("USAGE_TYPE"),
+		// 			},
+		// 		},
+		// 		Metrics: []*string{aws.String("BlendedCost")},
+		// 		TimePeriod: &costexplorer.DateInterval{
+		// 			Start: aws.String("2022-01-01"),
+		// 			End:   aws.String("2022-01-31"),
+		// 		},
+		// 	},
+		// 	want: &costexplorer.GetCostAndUsageOutput{
+		// 		DimensionValueAttributes: []*costexplorer.DimensionValuesWithAttributes{
+		// 			{
+		// 				Attributes: map[string]*string{
+		// 					"description": aws.String("dev"),
+		// 				},
+		// 				Value: aws.String("123456789012"),
+		// 			}, {
+		// 				Attributes: map[string]*string{
+		// 					"description": aws.String("prod"),
+		// 				},
+		// 				Value: aws.String("987654321098"),
+		// 			},
+		// 		},
+		// 		GroupDefinitions: []*costexplorer.GroupDefinition{{
+		// 			Type: aws.String("DIMENSION"),
+		// 			Key:  aws.String("LINKED_ACCOUNT"),
+		// 		}, {
+		// 			Type: aws.String("DIMENSION"),
+		// 			Key:  aws.String("SERVICE"),
+		// 		}, {
+		// 			Type: aws.String("DIMENSION"),
+		// 			Key:  aws.String("USAGE_TYPE"),
+		// 		}},
+		// 		ResultsByTime: []*costexplorer.ResultByTime{{
+		// 			Estimated: aws.Bool(false),
+		// 			Groups: []*costexplorer.Group{
+		// 				{
+		// 					Keys: []*string{aws.String("123456789012"), aws.String("EC2 - Other"), aws.String("APN1-EBS:VolumeUsage.gp2")},
+		// 					Metrics: map[string]*costexplorer.MetricValue{
+		// 						"BlendedCost": {
+		// 							Amount: aws.String("3.716129016"),
+		// 							Unit:   aws.String("USD"),
+		// 						},
+		// 					},
+		// 				}, {
+		// 					Keys: []*string{aws.String("987654321098"), aws.String("AWS CloudTrail"), aws.String("APN1-PaidEventsRecorded")},
+		// 					Metrics: map[string]*costexplorer.MetricValue{
+		// 						"BlendedCost": {
+		// 							Amount: aws.String("1.37946"),
+		// 							Unit:   aws.String("USD"),
+		// 						},
+		// 					},
+		// 				}, {
+		// 					Keys: []*string{aws.String("987654321098"), aws.String("AWS CloudTrail"), aws.String("USE1-PaidEventsRecorded")},
+		// 					Metrics: map[string]*costexplorer.MetricValue{
+		// 						"BlendedCost": {
+		// 							Amount: aws.String("1.52738"),
+		// 							Unit:   aws.String("USD"),
+		// 						},
+		// 					},
+		// 				},
+		// 			},
+		// 			TimePeriod: &costexplorer.DateInterval{
+		// 				End:   aws.String("2022-01-31"),
+		// 				Start: aws.String("2022-01-01"),
+		// 			},
+		// 			Total: map[string]*costexplorer.MetricValue{},
+		// 		}},
+		// 	},
+		// 	wantErr: false,
+		// },
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			mockCtrl := gomock.NewController(t)
+			defer mockCtrl.Finish()
+
+			mockCostExplorer := mock_retinacostexplorer.NewMockCostExplorer(mockCtrl)
+			tt.prepareMockFn(mockCostExplorer)
+
 			svc := New(mockCostExplorer)
 			got, err := svc.GetRetinaCostAndUsage(tt.args)
 			if (err != nil) != tt.wantErr {
