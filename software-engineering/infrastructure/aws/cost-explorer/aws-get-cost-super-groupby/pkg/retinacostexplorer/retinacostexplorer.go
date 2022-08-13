2bc0a67046de142211aa5fb9390f3ad3d6763845
xhiroga <13391129+xhiroga@users.noreply.github.com>
2022-08-13 20:58:44 +0900

    refactor(retina-ce): change module name

--- software-engineering/infrastructure/aws/cost-explorer/aws-get-cost-super-groupby/pkg/retinacostexplorer/retinacostexplorer.go
+++ software-engineering/infrastructure/aws/cost-explorer/aws-get-cost-super-groupby/pkg/retinacostexplorer/retinacostexplorer.go
@@ -1,4 +1,4 @@
-package ce
+package retinacostexplorer
 
 import (
 	"github.com/aws/aws-sdk-go/service/costexplorer"
@@ -12,21 +12,13 @@ type RetinaCostExplorer struct {
 	CostExplorer
 }
 
-func NewRetinaCostExplorer(ce CostExplorer) *RetinaCostExplorer {
+func New(ce CostExplorer) *RetinaCostExplorer {
 	return &RetinaCostExplorer{
 		CostExplorer: ce,
 	}
 }
 
 func (svc *RetinaCostExplorer) GetRetinaCostAndUsage(input *costexplorer.GetCostAndUsageInput) (*costexplorer.GetCostAndUsageOutput, error) {
+	// TODO
 	return svc.GetCostAndUsage(input)
 }
-
-// func TestGetRetinaCostAndUsage(t *testing.T) {
-// 	tests := []struct {
-// 		name    string
-// 		args    *costexplorer.GetCostAndUsageInput
-// 		want    *costexplorer.GetCostAndUsageOutput
-// 		wantErr bool
-// 	}{}
-// }
