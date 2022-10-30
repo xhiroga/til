c15879ce679285921b06216a3f4b26d01688d9e8
xhiroga <13391129+xhiroga@users.noreply.github.com>
2022-10-30 22:30:48 +0900

    feat: katakana

---
+++ software-engineering/standards/unicode/katakana/regex.ts
@@ -0,0 +1,20 @@
+import { Pattern } from "https://deno.land/x/regexbuilder@1.6.3/mod.ts";
+
+const regs = [
+    '^[ァ-ン]+$',  // https://gist.github.com/terrancesnyder/1345094
+    '^[ァ-ヾ]+$',  // https://gist.github.com/terrancesnyder/1345094?permalink_comment_id=2855970#gistcomment-2855970
+    '^[゠-ヿㇰ-ㇿﾟ]+$',
+]
+
+const test = (reg: string) => {
+    console.log(reg);
+    const pattern = Pattern.new().template("reg").vars({ reg }).build();
+    console.log(pattern.test('アイウエオ'));
+    console.log(pattern.test('イスヾ'));    // 踊り字
+    console.log(pattern.test('キャサリン・ゼタ゠ジョーンズ')); // [ダブルハイフン - Wikipedia](https://ja.wikipedia.org/wiki/%E3%83%80%E3%83%96%E3%83%AB%E3%83%8F%E3%82%A4%E3%83%95%E3%83%B3)
+    console.log(pattern.test('ボクハトテモキミニサイカイスルヿハデキヌトオモウ'));  // [ヿ - Wikipedia](https://ja.wikipedia.org/wiki/%E3%83%BF)
+    console.log(pattern.test('ピㇼカコタン'));  // [Unicode 片仮名拡張 - CyberLibrarian](https://www.asahi-net.or.jp/~ax2s-kmtn/ref/unicode/u31f0.html)
+    console.log(pattern.test('シンカﾟーソンクﾟライター'));  // [か゜ - Wikipedia](https://ja.wikipedia.org/wiki/%E3%81%8B%E3%82%9C)
+}
+
+regs.forEach(test);
