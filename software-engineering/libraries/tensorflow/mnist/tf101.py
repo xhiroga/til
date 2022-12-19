f494218d0d3d4f6261f8e7c35727f5231105aabe
xhiroga <13391129+xhiroga@users.noreply.github.com>
2022-12-20 06:17:16 +0900

    feat(tensorflow): format by black

--- software-engineering/libraries/tensorflow/mnist/tf101.py
+++ software-engineering/libraries/tensorflow/mnist/tf101.py
@@ -1,23 +1,26 @@
 import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
 
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+mnist = tf.keras.datasets.mnist
+
 
 def main():
 
-    print (type(mnist)) # これ何型？
+    print(type(mnist))  # これ何型？
 
     x = tf.placeholder(tf.float32, [None, 784])
     # シェイプは配列で表す
 
-    W = tf.Variable(tf.zeros([784,10]))
+    W = tf.Variable(tf.zeros([784, 10]))
     b = tf.Variable(tf.zeros([10]))
 
-    y = tf.nn.softmax(tf.matmul(x,W) + b)
+    y = tf.nn.softmax(tf.matmul(x, W) + b)
 
     # (コスト定義)交差エントロピーを定義する
-    y_ = tf.placeholder(tf.float32, [None,10])
-    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+    y_ = tf.placeholder(tf.float32, [None, 10])
+    cross_entropy = tf.reduce_mean(
+        -tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1])
+    )
     # y_は0or1, つまり1のはずのものを0.3Xとかで予想すると、ログは-XXXXとか巨大になり、ダメージがでかいようだ。
     # reduce_mean: 平均化によってランクをひとつ削減できるから？
 
@@ -28,22 +31,22 @@ def main():
     sess = tf.InteractiveSession()
     tf.global_variables_initializer().run()
 
-    print ("モデルによる予想 -> 誤差を重み付けに反映、を1000回繰り返す")
+    print("モデルによる予想 -> 誤差を重み付けに反映、を1000回繰り返す")
     for i in range(1000):
-      batch_xs, batch_ys = mnist.train.next_batch(100)
-      sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-      if (i%100==0):
-          print (str(i) + "周目の正答率...")
-          scoring(x, y, y_, sess)
+        if i % 100 == 0:
+            print(str(i) + "周目の正答率...")
+            scoring(x, y, y_, sess)
 
-    print ("正誤判定 -> 平均正答率を算出")
+    print("正誤判定 -> 平均正答率を算出")
     scoring(x, y, y_, sess)
 
 
 def scoring(x, y, y_, sess):
     # 正誤判定
-    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
     # このtf.argmaxが便利。[0.1,0.75,0.15]とかを"2"にしてくれる（一番数字の高いのを返す）
 
     # 平均を計算
