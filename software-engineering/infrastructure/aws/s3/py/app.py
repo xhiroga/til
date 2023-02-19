c73842503911d87d63180e2d484ad2ca23364ffa
xhiroga <13391129+xhiroga@users.noreply.github.com>
2023-02-19 10:12:55 +0900

    refactor: about s3

--- software-engineering/infrastructure/aws/s3/py/app.py
+++
@@ -1,9 +0,0 @@
-import os
-import tinys3
-# Simple Library to upload file to s3 made by Smore Inc. 
-
-conn = tinys3.Connection(os.environ["S3_ACCESS_KEY"], os.environ["S3_SECRET_KEY"], tls=True)
-
-f = open('README.md','rb')
-conn.upload('README.md',f,'s3.amazonaws.com/hiroga/shenzhen2018/')
-# なぜか .s3.amazonaws.com フォルダにアップロードされる
