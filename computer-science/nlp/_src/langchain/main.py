deff663ed0447dadd66312ef2cd737e8e09e8731
xhiroga <13391129+xhiroga@users.noreply.github.com>
2023-03-19 11:49:51 +0900

    chore: code but api slow

---
+++ computer-science/nlp/_src/langchain/main.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+from langchain.llms import OpenAI
+
+llm = OpenAI(temperature=0.9)
+print(llm("しりとりで使いたいので、「り」から始まって「り」で終わる言葉をいくつか挙げてください。"))
