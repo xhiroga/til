00e0f30583c4a688a2088a425c59f28d45f89558
xhiroga <xhiroga@users.noreply.github.com>
2023-04-22 16:26:32 +0900

    chore: debug llama-index

--- computer-science/nlp/_src/llama_index_local/issue_1194.py
+++ computer-science/nlp/_src/llama_index_local/issue_1194.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv
 from gpt_index import GithubRepositoryReader, GPTSimpleVectorIndex
 
 load_dotenv()
+
 loader = GithubRepositoryReader(
     github_token=os.environ["GITHUB_TOKEN"],
-    owner="jerryjliu",
-    repo="llama_index",
+    owner="xhiroga",
+    repo="dotfiles",
     use_parser=False,
     verbose=True,
-    ignore_directories=["examples"],
     concurrent_requests=1
 )
 
@@ -20,4 +20,4 @@ for doc in docs:
     print(doc.extra_info)
 
 index = GPTSimpleVectorIndex.from_documents(docs)
-index.save_to_disk('data/llama_index.index.json')
+index.save_to_disk("data/something.json")
