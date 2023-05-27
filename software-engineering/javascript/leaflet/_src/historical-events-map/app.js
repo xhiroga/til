03b83e4367a5275881a41f6a305b5904755dcce5
xhiroga <xhiroga@users.noreply.github.com>
2023-05-28 08:15:11 +0900

    feat: add image

--- software-engineering/javascript/leaflet/_src/historical-events-map/app.js
+++ software-engineering/javascript/leaflet/_src/historical-events-map/app.js
@@ -7,7 +7,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var geojsonLayer = L.geoJson().addTo(map);
 
-// Fetch your GeoJSON data once
 fetch('data.geojson')
     .then(response => response.json())
     .then(data => {
@@ -21,8 +20,28 @@ function updateMap(year) {
         ...geoJsonData,
         features: geoJsonData.features.filter(feature => feature.properties.year === parseInt(year))
     };
-    geojsonLayer.clearLayers();
-    geojsonLayer.addData(filteredData);
+
+    // Clear all layers
+    map.eachLayer(function (layer) {
+      if (layer !== L.tileLayer) map.removeLayer(layer);
+    });
+
+    // Add the filtered data to the map
+    filteredData.features.forEach(function(feature) {
+      var coordinates = feature.geometry.coordinates;
+      var image = feature.properties.image;
+      if (image) {
+        var icon = L.icon({
+          iconUrl: image,
+          iconSize: [38, 38], // size of the icon
+          iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+          popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+        });
+        L.marker([coordinates[1], coordinates[0]], {icon: icon}).addTo(map);
+      } else {
+        L.marker([coordinates[1], coordinates[0]]).addTo(map);
+      }
+    });
 
     // Display the current slider value
     document.getElementById("slider-value").innerText = year;
