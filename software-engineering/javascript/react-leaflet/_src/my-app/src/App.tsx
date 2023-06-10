cae0b929781807655e00b49a731438160ca094e5
xhiroga <xhiroga@users.noreply.github.com>
2023-06-11 07:36:27 +0900

    feat: react-leaflet v4 sample

---
+++ software-engineering/javascript/react-leaflet/_src/my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import 'leaflet/dist/leaflet.css';
+import { Map } from './components/Map';
+
+function App() {
+  return (
+    <div className="App">
+      <Map />
+    </div>
+  );
+}
+
+export default App;
