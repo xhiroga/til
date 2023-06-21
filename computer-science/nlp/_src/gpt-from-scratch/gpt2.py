137c855b89edf2de3e6d32ef4b7d4a6922747451
xhiroga <13391129+xhiroga@users.noreply.github.com>
2023-06-22 05:11:08 +0900

    chore(gpt-2): wip

--- computer-science/nlp/_src/gpt-from-scratch/gpt2.py
+++ computer-science/nlp/_src/gpt-from-scratch/gpt2.py
@@ -1,7 +1,31 @@
 import numpy as np
 
-def gpt2(inputs, params, n_head, n_tokens_to_generate):
-    pass # TODO: implement this
+
+def gpt2(inputs, wte, wpe, blocks, ln_f, n_head):  # [n_seq] -> [n_seq, n_vocab]
+    """
+    Parameters
+    ---
+    inputs: 
+    
+    wte: 
+        Word Token Embeddingsの略と思われる。
+        これも学習対象だと思っていたら、引数で渡されるのでよくわからない。
+
+    wpe:
+        Word Position Embeddingsの略と思われる。
+        これも外から渡されるの、よく分からないな...
+    """
+
+    # token + positional embeddings
+    x = wte[inputs] + wpe[range(len(inputs))]  # [n_seq] -> [n_seq, n_embd]
+
+    # forward pass through n_layer transformer blocks
+    for block in blocks:
+        x = transformer_block(x, **block, n_head=n_head)  # [n_seq, n_embd] -> [n_seq, n_embd]
+
+    # projection to vocab
+    x = layer_norm(x, **ln_f)  # [n_seq, n_embd] -> [n_seq, n_embd]
+    return x @ wte.T  # [n_seq, n_embd] -> [n_seq, n_vocab]
 
 def generate(inputs, params, n_head, n_tokens_to_generate):
     from tqdm import tqdm   # アラビア語のtaqadum(進歩・前進)に由来する、進捗バーを簡単に表示するライブラリ
