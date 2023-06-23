dc17659bc267206881c09534d96d9e6c5574b993
xhiroga <xhiroga@users.noreply.github.com>
2023-06-23 17:56:18 +0900

    feat(prisma): create and update document

--- software-engineering/javascript/prisma/_src/mongodb/index.ts
+++ software-engineering/javascript/prisma/_src/mongodb/index.ts
@@ -2,13 +2,55 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-async function main() {
-  // ... you will write your Prisma Client queries here
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+async function create() {
+  await prisma.user.create({
+    data: {
+      name: 'Rich',
+      email: 'hello@prisma.com',
+      posts: {
+        create: {
+          title: 'My first post',
+          body: 'Lots of really interesting stuff',
+          slug: 'my-first-post',
+        },
+      },
+    },
+  })
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  })
+  console.dir(allUsers, { depth: null })
 }
 
-main()
+async function update(){
+  await prisma.post.update({
+    where: {
+      slug: 'my-first-post',
+    },
+    data: {
+      comments: {
+        createMany: {
+          data: [
+            { comment: 'Great post!' },
+            { comment: "Can't wait to read more!" },
+          ],
+        },
+      },
+    },
+  })
+  const posts = await prisma.post.findMany({
+    include: {
+      comments: true,
+    },
+  })
+
+  console.dir(posts, { depth: Infinity })
+}
+
+update()
   .then(async () => {
     await prisma.$disconnect()
   })
