39eb5767cc76bc9a76ac003e6eaef29444f5b18e
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2024-01-12 21:48:11 +0900

    chore: (wip) CNN

---
+++ computer-science/machine-learning/_src/deep-learning-from-scratch/README.md
@@ -0,0 +1,5 @@
+# [O'Reilly Japan - ゼロから作るDeep Learning](https://www.oreilly.co.jp/books/9784873117584/)
+
+```powershell
+./latest.bat
+```
---
+++ computer-science/machine-learning/_src/deep-learning-from-scratch/latest.bat
@@ -0,0 +1,11 @@
+@echo off
+SET repo_path=deep-learning-from-scratch
+
+IF NOT EXIST "%repo_path%" (
+    echo Repository not found. Cloning...
+    git clone https://github.com/oreilly-japan/deep-learning-from-scratch.git
+) ELSE (
+    echo Repository found. Updating...
+    cd %repo_path%
+    git pull
+)
