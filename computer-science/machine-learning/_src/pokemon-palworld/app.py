bf6cc12e6f91d1e558b21ac82a4f273cd7a71ab3
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2024-02-13 08:13:23 +0900

    feat: app for resnet18 fine-tuned model

--- computer-science/machine-learning/_src/pokemon-palworld/app.py
+++ computer-science/machine-learning/_src/pokemon-palworld/app.py
@@ -2,44 +2,42 @@ import gradio as gr
 import torch
 from PIL import Image
 from safetensors import safe_open
-from torchvision import transforms
-from utils.SimpleCNN import SimpleCNN
+from torchvision import models, transforms
 
-image_size = 256
+labels = ["pal", "pokemon"]
 
-# Define the image transformation
-transform = transforms.Compose([
-    transforms.Resize((image_size, image_size)),
-    transforms.ToTensor(),
-])
+model = models.resnet18(pretrained=True)
+model.fc = torch.nn.Linear(model.fc.in_features, len(labels))
 
-# Load the trained model
 model_save_path = "models/model.safetensors"
 tensors = {}
 with safe_open(model_save_path, framework="pt", device="cpu") as f:
     for key in f.keys():
         tensors[key] = f.get_tensor(key)
 
-model = SimpleCNN(image_size=image_size)
-model.load_state_dict(tensors)
-
+model.load_state_dict(tensors, strict=False)
 model.eval()
 
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+
 def classify_image(input_image: Image):
-    # Convert the input image to tensor
-    input_tensor = transform(input_image)
-    input_tensor = input_tensor.unsqueeze(0)
+    img_t = preprocess(input_image)
+    batch_t = torch.unsqueeze(img_t, 0)
     
-    # Forward pass the input through the model
-    output = model(input_tensor)
-    print(output)
+    with torch.no_grad():
+        output = model(batch_t)
     
     probabilities = torch.nn.functional.softmax(output, dim=1)
-    
-    return {'Pokemon': probabilities[0][0], 'Pal': probabilities[0][1]}
 
-# Define the Gradio interface
+    label_to_prob = {labels[i]: prob for i, prob in enumerate(probabilities[0])}
+    return label_to_prob
+
+
 demo = gr.Interface(fn=classify_image, inputs=gr.Image(type='pil'), outputs='label')
-
-# Launch the application
 demo.launch()
