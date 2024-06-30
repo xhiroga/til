723989f14bc3ba19c5db60f51f1a41bb79171c01
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2024-07-01 08:58:13 +0900

    feat: Drawing by reinforcement learning

---
+++ computer-science/machine-learning/_src/rl-turtle/README.md
@@ -0,0 +1,7 @@
+# rl-turtle
+
+Drawing by reinforcement learning.
+
+```bash
+rye run python ./src/rl_turtle/td3.py
+```
---
+++ computer-science/machine-learning/_src/rl-turtle/src/rl_turtle/td3.py
@@ -0,0 +1,224 @@
+import datetime
+import io
+import os
+import turtle
+
+import cv2
+import gymnasium as gym
+import numpy as np
+import torch as th
+from PIL import Image
+from stable_baselines3 import TD3
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.noise import NormalActionNoise
+
+
+class DrawingEnv(gym.Env):
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
+
+    def __init__(self, target_image, render_mode=None, size=64):
+        # 公式には無かったが必要なのか？
+        super(DrawingEnv, self).__init__()
+
+        self.size = size
+        self.agent_image = np.zeros((self.size, self.size, 3), dtype=np.uint8)
+        image = Image.open(target_image).convert("RGB")
+
+        self.target_image = np.array(image.resize((self.size, self.size)))
+        self.target_image_transposed = self.target_image.transpose((2, 0, 1))
+        self.observation_space = gym.spaces.Dict(
+            {
+                "agent": gym.spaces.Box(
+                    low=0, high=255, shape=(3, self.size, self.size), dtype=np.uint8
+                ),
+                "target": gym.spaces.Box(
+                    low=0, high=255, shape=(3, self.size, self.size), dtype=np.uint8
+                ),
+            }
+        )
+
+        self.action_space_degree = 6
+        self.action_space = gym.spaces.Box(
+            low=0, high=1, shape=(self.action_space_degree,), dtype=np.float32
+        )
+
+        self.screen = turtle.Screen()
+        # ウィンドウサイズはウィンドウのマージン込なので、多めに取らないと全範囲が見えない
+        self.screen.setup(width=self.size + 64, height=self.size + 64)
+        self.agent = turtle.Turtle()
+
+        self.previous_mean_diff = -255
+
+        self.max_step = self.size / 2  # 勘
+
+        self.reset()
+
+    def _get_obs(self):
+        return {
+            "agent": self.agent_image.transpose((2, 0, 1)),
+            "target": self.target_image_transposed,
+        }
+
+    # 非常に難しい！
+    def _calc_reward(self):
+        img1_hsv = cv2.cvtColor(self.agent_image, cv2.COLOR_RGB2HSV)
+        img2_hsv = cv2.cvtColor(self.target_image, cv2.COLOR_RGB2HSV)
+        # 色相は0~180なのに、彩度と明度は0~255なので、単に平均を取っていいのか？という話はある
+        diff = cv2.absdiff(img1_hsv, img2_hsv)
+        mean_diff = np.mean(diff)
+        reward = mean_diff - self.previous_mean_diff
+        self.previous_mean_diff = mean_diff
+        return reward * 10
+
+    def reset(self, seed=None, options=None):
+        self.screen.clearscreen()
+        self.agent.reset()
+        self.agent.speed(0)
+        self._show_drawing_area()
+        self.agent.teleport(0, 0)
+        self.agent.pensize(self.size // 16 + 1)  # これも勘
+        self.current_step = 0
+
+        self.agent_image = np.zeros((self.size, self.size, 3), dtype=np.uint8)
+
+        return self._get_obs(), {}
+
+    # マジで便利、絶対実装したほうがいい。学習が膠着したときにフリーズしてるのかどうか、枠の再描画でひと目で判断できる
+    def _show_drawing_area(self):
+        self.agent.teleport(-(self.size / 2), -(self.size / 2))
+        self.agent.pensize(1)
+        for _ in range(4):
+            self.agent.forward(self.size)
+            self.agent.left(90)
+
+    def step(self, action):
+        (x, y, distance, r, g, b) = action
+        x = x * self.size - self.size / 2
+        y = y * self.size - self.size / 2
+        distance = distance * self.size
+
+        self.agent.teleport(x, y)
+        self.agent.pencolor(r, g, b)
+        self.agent.forward(distance)
+
+        self.current_step += 1
+
+        # setupは原点を中心にウインドウを展開したが、postscriptは原点から正方向に描画する上に、座標が左手系なので非常に分かりづらい...
+        ps = self.screen.getcanvas().postscript(
+            colormode="color",
+            x=-(self.size / 2),
+            y=-(self.size / 2),
+            width=self.size,
+            height=self.size,
+        )
+        # 念のためresizeしているがおそらく不要
+        self.agent_image = np.array(
+            Image.open(io.BytesIO(ps.encode("utf-8"))).resize((self.size, self.size))
+        )
+
+        reward = self._calc_reward()
+        observation = self._get_obs()
+        terminated = self.current_step >= self.max_step
+        return observation, reward, terminated, False, {}
+
+    def close(self):
+        self.screen.bye()
+
+
+class CustomCallBack(BaseCallback):
+    def __init__(self, task_name, verbose=0):
+        super(CustomCallBack, self).__init__(verbose)
+        self.log_dir = f"logs/{task_name}"
+        os.makedirs(self.log_dir, exist_ok=True)
+        self.before_update_param = None
+        self.update_counter = 0
+
+    def _on_rollout_start(self) -> None:
+        if self.before_update_param is not None:
+            self.compare_weights()
+
+        self.before_update_param = {
+            name: param.clone() for name, param in self.model.policy.named_parameters()
+        }
+
+    def _on_step(self) -> bool:
+        actions = self.locals["actions"]
+        buffer_actions = self.locals["buffer_actions"]
+        rewards = self.locals["rewards"]
+        message = (
+            f"n_calls={self.n_calls}, "
+            f"rewards={rewards}, "
+            f"actions={actions}, "
+            f"buffer_actions={buffer_actions}"
+        )
+        self._write_log(
+            "on_step",
+            message,
+        )
+        # `_on_step` は `reset` 呼び出し後に呼ばれるため、 `% 16 == 0` だと真っ黒な画面がログされる
+        if self.n_calls % 16 == 15:
+            new_obs = self.locals["new_obs"]
+            agent_image = new_obs["agent"]
+            if isinstance(agent_image, np.ndarray):
+                img_array = agent_image[0].transpose((1, 2, 0)).astype(np.uint8)
+                img = Image.fromarray(img_array)
+                img.save(f"{self.log_dir}/observation_{self.n_calls}.png")
+
+        return True
+
+    # _on_rollout_endの終了後にポリシーが更新されるため、このタイミングで利用可能になる新しい情報はない...はず...
+    def _on_rollout_end(self) -> None:
+        self.update_counter += 1
+
+    def compare_weights(self):
+        for name, param in self.model.policy.named_parameters():
+            before = self.before_update_param[name]
+            after = param.clone()
+            mean_diff = th.mean(after - before).item()
+            max_diff = th.max(after - before).item()
+            self._write_log(
+                "compare_weights",
+                f"Update #{self.update_counter} Layer {name} | {mean_diff=} | {max_diff=}",
+            )
+
+    def _write_log(self, file_name, message):
+        with open(f"{self.log_dir}/{file_name}.log", "a") as f:
+            f.write(message + "\n")
+
+
+target_image_path = "data/star.bmp"
+env = DrawingEnv(target_image_path)
+
+n_actions = env.action_space_degree
+action_noise = NormalActionNoise(
+    mean=np.zeros(n_actions), sigma=0.5 * np.ones(n_actions)
+)
+
+# buffer_sizeを指定しないと、`buffers.py:241: UserWarning: This system does not have apparently enough memory to store the complete replay buffer 24.61GB > 6.86GB` のようにエラーになる
+model = TD3(
+    "MultiInputPolicy",
+    env,
+    action_noise=action_noise,
+    buffer_size=1000,
+    learning_rate=0.01,  # 膠着を打破するためにデフォルトの10倍にしてみる
+    verbose=1,
+)
+model.learn(
+    total_timesteps=1000,
+    log_interval=4,
+    callback=CustomCallBack(
+        f"drawing_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}", verbose=1
+    ),
+)
+model.save("td3_drawing")
+
+del model  # remove to demonstrate saving and loading
+
+model = TD3.load("td3_drawing")
+
+obs, info = env.reset()
+while True:
+    action, _states = model.predict(obs, deterministic=True)
+    obs, reward, terminated, truncated, info = env.step(action)
+    if terminated or truncated:
+        obs, info = env.reset()
