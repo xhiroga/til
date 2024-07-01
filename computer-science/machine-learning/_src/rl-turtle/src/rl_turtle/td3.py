ba9a7d4e52ff28890333b8026afabd4e388ae8e6
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2024-07-01 11:47:13 +0900

    feat: mono

--- computer-science/machine-learning/_src/rl-turtle/src/rl_turtle/td3.py
+++ computer-science/machine-learning/_src/rl-turtle/src/rl_turtle/td3.py
@@ -2,7 +2,6 @@ import datetime
 import io
 import os
 import turtle
-from colorsys import hsv_to_rgb
 
 import cv2
 import gymnasium as gym
@@ -21,27 +20,28 @@ class DrawingEnv(gym.Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
     def __init__(self, target_image, render_mode=None, size=64):
-        # 公式には無かったが必要なのか？
         super(DrawingEnv, self).__init__()
 
         self.size = size
-        self.agent_image = np.zeros((self.size, self.size, 3), dtype=np.uint8)
-        image = Image.open(target_image).convert("RGB")
+        self.agent_image = np.zeros((self.size, self.size), dtype=np.uint8)
+        image = Image.open(target_image).convert("L")  # モノクロに変換
 
         self.target_image = np.array(image.resize((self.size, self.size)))
-        self.target_image_transposed = self.target_image.transpose((2, 0, 1))
+        self.target_image_transposed = self.target_image[
+            None, :, :
+        ]  # チャネル次元を追加
         self.observation_space = gym.spaces.Dict(
             {
                 "agent": gym.spaces.Box(
-                    low=0, high=255, shape=(3, self.size, self.size), dtype=np.uint8
+                    low=0, high=255, shape=(1, self.size, self.size), dtype=np.uint8
                 ),
                 "target": gym.spaces.Box(
-                    low=0, high=255, shape=(3, self.size, self.size), dtype=np.uint8
+                    low=0, high=255, shape=(1, self.size, self.size), dtype=np.uint8
                 ),
             }
         )
 
-        self.action_space_degree = 6
+        self.action_space_degree = 4
         self.action_space = gym.spaces.Box(
             low=0, high=1, shape=(self.action_space_degree,), dtype=np.float32
         )
@@ -59,21 +59,19 @@ class DrawingEnv(gym.Env):
 
     def _get_obs(self):
         return {
-            "agent": self.agent_image.transpose((2, 0, 1)),
+            "agent": self.agent_image[None, :, :],  # チャネル次元を追加
             "target": self.target_image_transposed,
         }
 
-    # 非常に難しい！
     def _calc_reward(self):
-        img1_hsv = cv2.cvtColor(self.agent_image, cv2.COLOR_RGB2HSV)
-        img2_hsv = cv2.cvtColor(self.target_image, cv2.COLOR_RGB2HSV)
-        # 色相は0~180なのに、彩度と明度は0~255なので、単に平均を取っていいのか？という話はある
-        diff = cv2.absdiff(img1_hsv, img2_hsv)
+        diff = cv2.absdiff(self.agent_image, self.target_image)
         mean_diff = np.mean(diff)
         improvement = mean_diff - self.previous_mean_diff
+        wow = improvement - 255 / self.max_step
         self.previous_mean_diff = mean_diff
-        # TODO: 目標達成したときのrewardもどこかで加えたほうが良い
-        return improvement if improvement > 0 else -mean_diff
+        # 単にimprovementを用いると途中からほとんど線を引かなくなる（目標が高すぎ現象）
+        # improvement if improvement > 0 else -mean_diff とすると、自分で黒い線を引く → 白い線で上書きする、でスコアを稼げてしまう...
+        return wow
 
     def reset(self, seed=None, options=None):
         self.screen.clearscreen()
@@ -84,30 +82,30 @@ class DrawingEnv(gym.Env):
         self.agent.teleport(0, 0)
         self.agent.pensize(self.size // 16 + 1)  # これも勘
         self.current_step = 0
-        self.previous_mean_diff = 255
 
-        self.agent_image = np.zeros((self.size, self.size, 3), dtype=np.uint8)
+        self.agent_image = np.ones((self.size, self.size), dtype=np.uint8)
+        self.previous_mean_diff = self._calc_reward()
 
         return self._get_obs(), {}
 
     # マジで便利、絶対実装したほうがいい。学習が膠着したときにフリーズしてるのかどうか、枠の再描画でひと目で判断できる
     def _show_drawing_area(self):
-        self.agent.teleport(-(self.size / 2), -(self.size / 2))
+        # 枠をキャプチャ領域より広く実装しないと、枠を白塗りで消すのが最適行動になってしまう
+        self.agent.teleport(self.size / 2 + 1, self.size / 2 + 1)
         self.agent.pensize(1)
         for _ in range(4):
-            self.agent.forward(self.size)
-            self.agent.left(90)
+            self.agent.right(90)
+            self.agent.forward(self.size + 2)
 
     def step(self, action):
-        (x, y, distance, h, s, v) = action
+        (x, y, distance, gray) = action
         x = x * self.size - self.size / 2
-        y = y * self.size - self.size / 2
-        # action spaceをHSVで定義したほうが明度が似る
-        r, g, b = hsv_to_rgb(h, s, v)
+        # おまじない程度だが、アクションはCNNに合わせて右手系になっていると解釈し、step内で左手系に直す
+        y = self.size / 2 - y * self.size
         distance = distance * self.size
 
         self.agent.teleport(x, y)
-        self.agent.pencolor(r, g, b)
+        self.agent.pencolor(gray, gray, gray)  # モノクロに設定
         self.agent.forward(distance)
 
         self.current_step += 1
@@ -120,9 +118,10 @@ class DrawingEnv(gym.Env):
             width=self.size,
             height=self.size,
         )
-        # 念のためresizeしているがおそらく不要
         self.agent_image = np.array(
-            Image.open(io.BytesIO(ps.encode("utf-8"))).resize((self.size, self.size))
+            Image.open(io.BytesIO(ps.encode("utf-8")))
+            .convert("L")
+            .resize((self.size, self.size))
         )
 
         reward = self._calc_reward()
@@ -164,13 +163,13 @@ class CustomCallBack(BaseCallback):
             "on_step",
             message,
         )
-        # `_on_step` は `reset` 呼び出し後に呼ばれるため、 `% 16 == 0` だと真っ黒な画面がログされる
         if self.n_calls % 16 == 15:
             new_obs = self.locals["new_obs"]
             agent_image = new_obs["agent"]
             if isinstance(agent_image, np.ndarray):
-                img_array = agent_image[0].transpose((1, 2, 0)).astype(np.uint8)
-                img = Image.fromarray(img_array)
+                img_array = agent_image[0].astype(np.uint8)
+                img_array = img_array.squeeze()  # 形状を (height, width) に変更
+                img = Image.fromarray(img_array, mode="L")  # モノクロとして保存
                 img.save(f"{self.log_dir}/observation_{self.n_calls}.png")
 
         return True
@@ -195,9 +194,8 @@ class CustomCallBack(BaseCallback):
             f.write(message + "\n")
 
 
-target_image_path = "data/star.bmp"
+target_image_path = "data/monostar.bmp"
 env = DrawingEnv(target_image_path)
-
 
 policy_type = "MultiInputPolicy"
 action_noise = NormalActionNoise(
@@ -219,7 +217,7 @@ run = wandb.init(
     project="rl-turtle",
     sync_tensorboard=True,
     config=config,
-    name="reward=(improvement if improvement > 0 else -mean_diff), action=(x, y, distance, h, s, v)",
+    name="reward=wow, action=(x, y, distance, gray)",
     save_code=True,
 )
 
@@ -243,15 +241,3 @@ model.learn(
     ],
 )
 run.finish()
-# model.save("td3_drawing")
-
-# del model  # remove to demonstrate saving and loading
-
-# model = TD3.load("td3_drawing")
-
-# obs, info = env.reset()
-# while True:
-#     action, _states = model.predict(obs, deterministic=True)
-#     obs, reward, terminated, truncated, info = env.step(action)
-#     if terminated or truncated:
-#         obs, info = env.reset()
