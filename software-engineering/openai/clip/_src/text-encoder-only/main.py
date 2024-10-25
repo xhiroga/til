d5551572846953121e7ee5c6218bf014fd70c89a
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2024-10-25 18:50:56 +0900

    docs: original code

--- software-engineering/openai/clip/_src/text-encoder-only/main.py
+++ software-engineering/openai/clip/_src/text-encoder-only/main.py
@@ -3,10 +3,12 @@ import clip
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model, _ = clip.load("ViT-B/32", device=device)
+
+# Original code refers self.visual.conv1.weight.dtype
 model.__class__.dtype = property(lambda obj: obj.transformer.resblocks[0].attn.in_proj_weight.dtype)
 
 text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
 
 with torch.no_grad():
     text_features = model.encode_text(text)
-    print(f"{text_features.shape}")
+    print(f"{text_features.shape=}")
