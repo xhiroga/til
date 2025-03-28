ffa546d70fa77ad34f76fbd3e73bdcefb70f6b43
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2025-03-28 18:28:33 +0900

    revert svgo

--- software-engineering/javascript/pixijs/_src/pull-the-pin/vite.config.ts
+++ software-engineering/javascript/pixijs/_src/pull-the-pin/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
-import svgo from "vite-plugin-svgo";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [viteSingleFile(), svgo()],
+  plugins: [viteSingleFile()],
   build: {
     assetsInlineLimit: 0, // Inline all assets
   },
