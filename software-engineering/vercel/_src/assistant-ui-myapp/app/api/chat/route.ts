d683f0d139570bdb1a9e873be47aa1ba475f778f
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2025-05-11 13:53:09 +0900

    feat: mount mcp

--- software-engineering/vercel/_src/assistant-ui-myapp/app/api/chat/route.ts
+++ software-engineering/vercel/_src/assistant-ui-myapp/app/api/chat/route.ts
@@ -1,25 +1,33 @@
 import { openai } from "@ai-sdk/openai";
-import { jsonSchema, streamText } from "ai";
+import { experimental_createMCPClient, streamText } from "ai";
+import { StreamableHTTPClientTransport } from '@modelcontextprotocol/sdk/client/streamableHttp.js';
+
 
 export const runtime = "edge";
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
-  const { messages, system, tools } = await req.json();
+  const { messages, system } = await req.json();
+
+  let tools = {};
+
+  // https://ai-sdk.dev/cookbook/next/mcp-tools
+  try {
+    const transport = new StreamableHTTPClientTransport(
+      new URL('http://localhost:8000/mcp/'),
+    );
+    const client = await experimental_createMCPClient({ transport })
+    tools = await client.tools()
+  } catch (error) {
+    console.error(error);
+  }
 
   const result = streamText({
     model: openai("gpt-4o"),
     messages,
     // forward system prompt and tools from the frontend
     system,
-    tools: Object.fromEntries(
-      Object.entries<{ parameters: unknown }>(tools).map(([name, tool]) => [
-        name,
-        {
-          parameters: jsonSchema(tool.parameters!),
-        },
-      ]),
-    ),
+    tools,
   });
 
   return result.toDataStreamResponse();
