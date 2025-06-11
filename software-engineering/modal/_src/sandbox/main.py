24f1eb4d099ef5633d763c45eabc72d6632a3ea1
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2025-06-11 15:40:35 +0900

    feat: main.pyを更新し、関数fを追加してローカルおよびリモートでの実行をサポート; Makefileを新規作成; 不要なREADME.mdを削除; pyproject.tomlを整理

--- software-engineering/modal/_src/sandbox/main.py
+++ software-engineering/modal/_src/sandbox/main.py
@@ -1,14 +1,34 @@
+import sys
+
 import modal
 
-app = modal.App("example-get-started")
-
+app = modal.App("example-hello-world")
 
 @app.function()
-def square(x):
-    print("This code is running on a remote worker!")
-    return x**2
+def f(i):
+    if i % 2 == 0:
+        print("hello", i)
+    else:
+        print("world", i, file=sys.stderr)
 
+    return i * i
 
 @app.local_entrypoint()
 def main():
-    print("the square is", square.remote(42))
+    # run the function locally
+    print(f.local(1000))
+
+    # run the function remotely on Modal
+    print(f.remote(1000))
+
+    # run the function in parallel and remotely on Modal
+    total = 0
+    for ret in f.map(range(200)):
+        total += ret
+
+    print(total)
+
+
+if __name__ == "__main__":
+    with app.run():
+        main()
