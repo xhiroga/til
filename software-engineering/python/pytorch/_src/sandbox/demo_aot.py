6cb9e05fa4b7a0e94b37492a6ffe52bcc7f5a0ab
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2025-06-13 17:40:22 +0900

    feat: demo_aot.pyにモデルの推論機能を追加し、エクスポートされたモデルの読み込みとベンチマークを実施; Makefileとpyproject.tomlを更新し、エクスポートされたモデルのパスを修正

--- software-engineering/python/pytorch/_src/sandbox/demo_aot.py
+++ software-engineering/python/pytorch/_src/sandbox/demo_aot.py
@@ -10,6 +10,39 @@ if not torch.cuda.is_available():
     raise RuntimeError("CUDA is not available")
 
 
+def export_compiled(model, input_tensor, exported_model_path):
+    print(f"Compiling and export to {exported_model_path}...")
+    input_tensor_cuda = input_tensor.to("cuda")
+    model.to("cuda")
+    compiled_model = torch_tensorrt.compile(
+        model, ir="dynamo", inputs=[input_tensor_cuda]
+    )
+    os.makedirs(os.path.dirname(exported_model_path), exist_ok=True)
+    torch_tensorrt.save(compiled_model, exported_model_path, inputs=[input_tensor_cuda])
+
+
+def inference(model, input_tensor, exported_model_path):
+    if not os.path.exists(exported_model_path):
+        print(f"{exported_model_path} not found. Please run with --export first.")
+        return
+
+    print("Running ResNet50 benchmark (original vs compiled & exported)...")
+    input_tensor.to("cuda")
+    model.to("cuda")
+    original_model_time = benchmark(model, input_tensor)
+    print(f"Original model average inference time: {original_model_time:.3f} ms")
+
+    print(f"Loading exported compiled TensorRT model from {exported_model_path}...")
+    loaded_exported_program = torch.export.load(exported_model_path).module()
+    print("Compiled TensorRT model loaded successfully.")
+
+    exported_model_time = benchmark(loaded_exported_program, input_tensor)
+    print(f"Compiled model average inference time: {exported_model_time:.3f} ms")
+
+    speedup = original_model_time / exported_model_time
+    print(f"Speedup: {speedup:.2f}x")
+
+
 def benchmark(model, input_tensor, num_runs=100):
     input_tensor = input_tensor.to("cuda")
     for _ in range(10):  # Warmup
@@ -26,60 +59,27 @@ def main():
         description="PyTorch AOT Autograd Demo with ResNet50"
     )
     parser.add_argument(
-        "--export", action="store_true", help="Export the model to an .pt2 file"
+        "--export", action="store_true", help="Export the model to models/trt.ep"
     )
     args = parser.parse_args()
 
     batch_size = 64
-    dummy_input = torch.randn(batch_size, 3, 224, 224)
+    input_tensor = torch.randn(batch_size, 3, 224, 224)
     model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
     model.eval()
 
-    exported_model_path = "models/exported_trt.pt2"
+    exported_model_path = "models/trt.ep"
 
     if args.export:
         try:
-            print("Compiling the model with Torch-TensorRT...")
-            dummy_input_cuda = dummy_input.to("cuda")
-            model.to("cuda")
-            compiled_model = torch_tensorrt.compile(
-                model, ir="dynamo", inputs=[dummy_input_cuda]
-            )
-            print("Model compiled successfully with Torch-TensorRT.")
-
-            print(f"Exporting compiled TensorRT model to {exported_model_path}...")
-            torch_tensorrt.save(
-                compiled_model, exported_model_path, inputs=[dummy_input_cuda]
-            )
-            os.makedirs(os.path.dirname(exported_model_path), exist_ok=True)
-            print("Compiled TensorRT model exported successfully.")
+            export_compiled(model, input_tensor, exported_model_path)
         except Exception as e:
             print(f"Error during compiled model export: {e}")
             if os.path.exists(exported_model_path):
                 os.remove(exported_model_path)
             return
     else:
-        if not os.path.exists(exported_model_path):
-            print(
-                f"Compiled model not found at {exported_model_path}. Please run with --export first."
-            )
-            return
-
-        print("Running ResNet50 benchmark (original vs compiled & exported)...")
-        dummy_input.to("cuda")
-        model.to("cuda")
-        original_model_time = benchmark(model, dummy_input)
-        print(f"Original model average inference time: {original_model_time:.3f} ms")
-
-        print(f"Loading exported compiled TensorRT model from {exported_model_path}...")
-        loaded_exported_program = torch.export.load(exported_model_path).module()
-        print("Compiled TensorRT model loaded successfully.")
-
-        exported_model_time = benchmark(loaded_exported_program, dummy_input)
-        print(f"Compiled model average inference time: {exported_model_time:.3f} ms")
-
-        speedup = original_model_time / exported_model_time
-        print(f"Speedup: {speedup:.2f}x")
+        inference(model, input_tensor, exported_model_path)
 
 
 if __name__ == "__main__":
