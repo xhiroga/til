3f15f139b86eef97c3a2a86e7287876a9783284b
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2025-06-18 07:56:07 +0900

    feat: demo_profile_table.pyを新規作成し、ResNet18モデルのパフォーマンスをプロファイリングする機能を追加

---
+++ software-engineering/python/pytorch/_src/sandbox/demo_profile_table.py
@@ -0,0 +1,15 @@
+import torch
+import torchvision.models as models
+from torch.profiler import ProfilerActivity, profile
+
+model = models.resnet18()
+inputs = torch.randn(5, 3, 224, 224)
+
+with profile(
+    activities=[ProfilerActivity.CPU], profile_memory=True, record_shapes=True
+) as prof:
+    model(inputs)
+
+# prof オブジェクトに with ブロック外でアクセスしているが、これが正解。
+# ブロック内でアクセスすると何も表示されない。
+print(prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=10))
