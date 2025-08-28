9d10e931889ad323bc78c908fc800a8a729efd7c
Hiroaki Ogasawara <13391129+xhiroga@users.noreply.github.com>
2025-08-28 11:58:27 +0900

    chore: serve

---
+++ software-engineering/python/bentoml/_src/tutorial/service.py
@@ -0,0 +1,17 @@
+import bentoml
+
+@bentoml.service(
+    image=bentoml.images.Image(python_version="3.13").python_packages("torch", "transformers"),
+)
+class Summarization:
+    def __init__(self) -> None:
+        import torch
+        from transformers import pipeline
+
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.pipeline = pipeline('summarization', device=device)
+
+    @bentoml.api(batchable=True)
+    def summarize(self, texts: list[str]) -> list[str]:
+        results = self.pipeline(texts)
+        return [item['summary_text'] for item in results]
